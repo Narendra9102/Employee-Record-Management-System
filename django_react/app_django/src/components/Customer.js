4dbc9e1ae5d6f6c994716956ef1b78d797a497a8
Narendra Sompalli <141741512+Narendra9102@users.noreply.github.com>
2023-11-13 22:48:29 +0530

    Add files via upload

---
+++ django_react/app_django/src/components/Customer.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Customer = () => {
+  return (
+    <div>
+      <h1>Customer Page</h1>
+      <p>This is the customer page content.</p>
+    </div>
+  );
+};
+
+export default Customer;
