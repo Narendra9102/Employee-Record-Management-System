32dfd9eadb145380beea3df68bcb9641cdd1895b
Narendra Sompalli <141741512+Narendra9102@users.noreply.github.com>
2024-03-14 18:58:47 +0530

    Delete django_react directory

--- django_react/members/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-from .serializer import ProductSerializer
-from rest_framework import viewsets
-
-
-class ProductView(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
